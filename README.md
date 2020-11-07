8c2848c2b02bc9e2b4498ca315747f9d6dd12003
Mario Thompson <MarioThompson0010@gmail.com>
2020-11-06 20:35:49 -0600

    Create new README for recording

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 
   ![license](https://img.shields.io/badge/License-Apache-blue)
-  # Hey
+  # My Awesome README
   
   ## Description: 
-  My description
+  This software creates a readme.  Using a back-end tool and technology like inquirer, we can create prompts that enable users to do cool things like create readmes easily.  It asks for information, then, using backtick technology, this software puts values where they're supposed to go.  You can create a template. It's like a template.  
   
   ## Table of Contents
   
@@ -15,26 +15,73 @@
   * [Questions](https://github.com/MarioThompson0010/09_Generate_README#questions)
   
   ## Installation
-  you don't
+  You install this product by downloading the 09_Generate_README folder.  Open the project by typing code . in the bash interface. Then you right-click on the injex.js file and select "open in integrated terminal."  Then type "node index.js" and begin  to answer questions.
 
   ## Usage
-  press buttons
+  You answer the questions as they are asked of you.
 
   ## License
-  Apache :  Apache License
-            Version 2.0, January 2004
-            http://www.apache.org/licenses/
+  Apache :                       Apache License
+                           Version 2.0, January 2004
+                        http://www.apache.org/licenses/
+
+   TERMS AND CONDITIONS FOR USE, REPRODUCTION, AND DISTRIBUTION
+
+   1. Definitions.
+
+      "License" shall mean the terms and conditions for use, reproduction,
+      and distribution as defined by Sections 1 through 9 of this document.
+
+      "Licensor" shall mean the copyright owner or entity authorized by
+      the copyright owner that is granting the License.
+
+      "Legal Entity" shall mean the union of the acting entity and all
+      other entities that control, are controlled by, or are under common
+      control with that entity. For the purposes of this definition,
+      "control" means (i) the power, direct or indirect, to cause the
+      direction or management of such entity, whether by contract or
+      otherwise, or (ii) ownership of fifty percent (50%) or more of the
+      outstanding shares, or (iii) beneficial ownership of such entity.
+
+   
+
+   APPENDIX: How to apply the Apache License to your work.
+
+      To apply the Apache License to your work, attach the following
+      boilerplate notice, with the fields enclosed by brackets "[]"
+      replaced with your own identifying information. (Don't include
+      the brackets!)  The text should be enclosed in the appropriate
+      comment syntax for the file format. We also recommend that a
+      file or class name and description of purpose be included on the
+      same "printed page" as the copyright notice for easier
+      identification within third-party archives.
+
+   Copyright [yyyy] [name of copyright owner]
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+   
+   
 
   ## Contributing
-  Just contribute
+  You may not contribute to this product.
   
   ## Tests
-  run npm
+  Test this product by typing in "node index.js" in the terminal.  
   
   ## Questions
-  Username: jjjjlll
+  Username: MarioThompson0010
 
-  Email: jjjjjlkkkk
+  Email: MarioThompson0010@gmail.com
 
 
 
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,6 @@
 // function to generate markdown for README
 const licenseguy = {
-  MIT : `MIT License
+  MIT: `MIT License
 
   Copyright (c) 2020 Mario Thompson
   
@@ -9,33 +9,109 @@ const licenseguy = {
   The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
   
   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.`,
-  GNU:  `GNU GENERAL PUBLIC LICENSE
+
+  GNU: `
+                            GNU GENERAL PUBLIC LICENSE
          Version 3, 29 June 2007
-         Copyright (C) 2007 Free Software Foundation, Inc. https://fsf.org/ Everyone is permitted to copy and distribute verbatim copies of this license document, but changing it is not allowed.`,
-  Apache: ` Apache License
-            Version 2.0, January 2004
-            http://www.apache.org/licenses/`
+         Copyright (C) 2007 Free Software Foundation, Inc. https://fsf.org/ 
+         Everyone is permitted to copy and distribute verbatim copies of this license document, 
+         but changing it is not allowed. 
+         
+         2. Basic Permissions.
+
+         All rights granted under this License are granted for the term of
+       copyright on the Program, and are irrevocable provided the stated
+       conditions are met.  This License explicitly affirms your unlimited
+       permission to run the unmodified Program.  The output from running a
+       covered work is covered by this License only if the output, given its
+       content, constitutes a covered work.  This License acknowledges your
+       rights of fair use or other equivalent, as provided by copyright law.
+       
+         You may make, run and propagate covered works that you do not
+       convey, without conditions so long as your license otherwise remains
+       in force.  You may convey covered works to others for the sole purpose
+       of having them make modifications exclusively for you, or provide you
+       with facilities for running those works, provided that you comply with
+       the terms of this License in conveying all material for which you do
+       not control copyright.  Those thus making or running the covered works
+       for you must do so exclusively on your behalf, under your direction
+       and control, on terms that prohibit them from making any copies of
+       your copyrighted material outside their relationship with you.
+       
+         Conveying under any other circumstances is permitted solely under
+       the conditions stated below.  Sublicensing is not allowed; section 10
+       makes it unnecessary.` ,
+
+  Apache: `                      Apache License
+                           Version 2.0, January 2004
+                        http://www.apache.org/licenses/
+
+   TERMS AND CONDITIONS FOR USE, REPRODUCTION, AND DISTRIBUTION
+
+   1. Definitions.
+
+      "License" shall mean the terms and conditions for use, reproduction,
+      and distribution as defined by Sections 1 through 9 of this document.
+
+      "Licensor" shall mean the copyright owner or entity authorized by
+      the copyright owner that is granting the License.
+
+      "Legal Entity" shall mean the union of the acting entity and all
+      other entities that control, are controlled by, or are under common
+      control with that entity. For the purposes of this definition,
+      "control" means (i) the power, direct or indirect, to cause the
+      direction or management of such entity, whether by contract or
+      otherwise, or (ii) ownership of fifty percent (50%) or more of the
+      outstanding shares, or (iii) beneficial ownership of such entity.
+
+   
+
+   APPENDIX: How to apply the Apache License to your work.
+
+      To apply the Apache License to your work, attach the following
+      boilerplate notice, with the fields enclosed by brackets "[]"
+      replaced with your own identifying information. (Don't include
+      the brackets!)  The text should be enclosed in the appropriate
+      comment syntax for the file format. We also recommend that a
+      file or class name and description of purpose be included on the
+      same "printed page" as the copyright notice for easier
+      identification within third-party archives.
+
+   Copyright [yyyy] [name of copyright owner]
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+   
+   `
 }
 
 function generateMarkdown(data) {
 
-  const {MIT, GNU, Apache} = licenseguy;
+  const { MIT, GNU, Apache } = licenseguy;
   let licensedescript = '';
-  switch(data.LicensePrompt)
-  {
-    case  "MIT": {
+  switch (data.LicensePrompt) {
+    case "MIT": {
       licensedescript = MIT;
       break;
     }
-    case "GNU" : {
+    case "GNU": {
       licensedescript = GNU;
       break;
     }
-    case "Apache" : {
+    case "Apache": {
       licensedescript = Apache;
       break;
     }
-    default : {
+    default: {
       licensedescript = "unknown license";
     }
   }
@@ -86,5 +162,5 @@ function generateMarkdown(data) {
 
 module.exports = {
 
- generateMarkdown
+  generateMarkdown
 };
